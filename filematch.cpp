5710f930a3bf96013b0ceee0d5e3b2ac3095ed25
Jason Eslick <jslick@gmail.com>
2013-11-23 21:58:22 -0600

    Implement matching

--- filematch.cpp
+++ filematch.cpp
@@ -1,13 +1,56 @@
 #include "filematch.h"
 
+#include <functional>
+#include <regex>
+#include <boost/algorithm/string/replace.hpp>
 #include <boost/filesystem.hpp>
 
-static bool always_true(const boost::filesystem::path& path)
+static bool match_tautology(const boost::filesystem::path& path)
 {
     return true;
 }
 
+static bool match_expansion(const std::regex& regex, const boost::filesystem::path& path)
+{
+    using namespace std;
+
+    smatch match;
+    return regex_match(path.filename().string(), match, regex);
+}
+
+static std::string escape_regex(std::string str)
+{
+    using namespace boost::algorithm;
+
+    replace_all(str, "\\", "\\\\");   // must come first
+    replace_all(str, "*", "\\*");
+    replace_all(str, ".", "\\.");
+    replace_all(str, "+", "\\+");
+    replace_all(str, "/", "\\/");
+    replace_all(str, "?", "\\?");
+    replace_all(str, "(", "\\(");
+    replace_all(str, ")", "\\)");
+    replace_all(str, "[", "\\[");
+    replace_all(str, "]", "\\]");
+    replace_all(str, "{", "\\{");
+    replace_all(str, "}", "\\}");
+    return str;
+}
+
 Matcher create_matcher(const std::string& needle, MatchType match_type)
 {
-    return always_true;
+    using namespace std;
+    using namespace std::placeholders;
+    using namespace std::regex_constants;
+
+    if (needle.length())
+    {
+        string expansion = escape_regex(needle);
+        boost::algorithm::replace_all(expansion, "\\*", ".*");
+
+        regex needle_regex = regex(expansion, ECMAScript | icase);
+        return std::bind(&match_expansion, needle_regex, _1);
+    }
+    else
+        return match_tautology;
 }
